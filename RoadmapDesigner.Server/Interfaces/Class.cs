f3b63043485084b89eadd0af91c9ea4b7eaeafdf
HRV220 <rkhozhanets.1910@mail.ru>
2024-11-14 01:08:48 +0700

    Разделение логики программы на сервисы и ропощитории

---
+++ RoadmapDesigner.Server/Interfaces/Class.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RoadmapDesigner.Server.Models.EntityDTO;
+
+public interface IProgramVersionService
+{
+    Task<IEnumerable<ProgramVersionDTO>> GetAllProgramVersionsAsync();
+    Task<ProgramVersionDetailDTO?> GetProgramVersionDetailsAsync(Guid programVersionId);
+}
